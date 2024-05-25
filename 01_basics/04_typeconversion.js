47acced9d2a886e40b7a18458a9c85f0b88489ea
Puneetchauhan9 <pc3622433@gmail.com>
2024-05-25 16:59:33 +0000

    04_typeconversion

---
+++ 01_basics/04_typeconversion.js
@@ -0,0 +1,35 @@
+/*let isLoggedIn = 1
+console.log(typeof isLoggedIn );
+console.log(typeof (isLoggedIn) );
+
+let change = ("isLoggedIn");
+console.log(typeof change );
+
+//let changeInNumber = Number(isLoggedIn)
+console.log(typeof changeInNumber);
+
+console.log( typeof isLoggedIn);
+let changeInNumber = Boolean(isLoggedIn)
+console.log(changeInNumber);*/
+
+//"33"=>33
+//"33abc"=> NaN(not a number)
+// true=> 1;  false=> 0;
+
+//"" => false
+//puneet => true
+
+let someNumber = "puneet"
+console.log(typeof someNumber);
+
+let convert = Boolean(someNumber)
+console.log(convert);
+
+//""=>false
+//"puneet"=>true
+
+
+
+
+
+
