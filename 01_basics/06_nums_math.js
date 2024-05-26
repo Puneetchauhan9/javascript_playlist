0cabe9afaaec52cf0db24be47bf8671c2962bd91
Puneetchauhan9 <pc3622433@gmail.com>
2024-05-26 14:20:23 +0000

    06_nums_math.

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,21 @@
+// const score = 500
+// console.log(score);
+
+const Balance = new Number(502.78)
+// console.log(Balance);
+
+// console.log(Balance.toString())
+
+//console.log(Balance.toFixed(2));//used to fixed the value
+// console.log(Balance.toPrecision(3));
+//expected output:500.8
+//for (3) expected output is 501
+
+const Rupees = 1000000
+console.log(Rupees.toLocaleString('en-IN'))//without descibe standard it answer in us standard but "en-IN"
+//is indian standard so commas according ti indian system 10,00,000...
+
+
+//*******************************************Maths*****************************************************
+
+
