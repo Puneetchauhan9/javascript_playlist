2f275def11217ad3b3cf9afc1c971b552cda96c3
Puneetchauhan9 <pc3622433@gmail.com>
2024-05-26 13:35:10 +0000

    05_strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -14,9 +14,68 @@ let sentence = "my name is puneet singh chauhan"
 
 
 let index = 4;
-console.log(`Charcter code  ${sentence1.charCodeAt(index)} is equal to ${sentence1.charAt(index)}`)
+//console.log(`Charcter code  ${sentence1.charCodeAt(index)} is equal to ${sentence1.charAt(index)}`)
 //expected outpur:charcter code 97 is equal to a.
   
+const str1 ='puneet'
+const str2 = 'chauhan'
+
+//console.log(str2.concat(' ',str1));
+//expected output is chauhan puneet.
+
+const a = String("puneet")
+const b = new String("puneet")
+//console.log(typeof a);//here a is string
+//console.log(typeof b);//whereas b is object of output [String:'puneet']
+
+const str3 = "Puneet chauhan is a good boy.And his surname is chauhan "
+//console.log(str3.endsWith('boy',28))
+//expected output true
+
+//let word = "chauhan"
+
+//console.log(`the word "${word}"${str3.includes(word) ? 'is': 'is not' } ,in the sentence`);
+
+let word = "chauhan"
+// let indexOfFirst =str3.indexOf(word);
+// console.log(indexOfFirst);
+
+//console.log(`The index of ${word} is ${str3.indexOf(word)}`);
+
+//console.log(`The last index of ${word} is ${str3.lastIndexOf(word)}`);
+//Expected output is 48...
+// const regex = /[A-Z]/g;
+// const found =  str3.match(regex);
+// console.log(found);
+// //expected output:['P', 'A']
+ 
+
+//console.log(str3.toUpperCase());
+//used to uppercase all the whole string...
+
+//console.log(str3.toLowerCase());
+//used to show whole string in lower case...
+
+//console.log(str3.substring(0 , 5));
+//expected output:punee
+
+//console.log(str3.length)//length is not a function...
+ const str4 = "    chauhan shab   "
+// console.log(str4.slice( 0 , 5));
+// console.log(str4);
+// console.log(str4.trim())//trim used to cut unwanted spaces...
+
+
+// const url ="https:www.pappu%20modi.com";
+// console.log(url.replace('%20' , '-'));
+// console.log(url.replace('pappu','chappu'));
+
+
+//``:symbol name backtics
+
+
+
+
 
 
 
