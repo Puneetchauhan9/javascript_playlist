6cbdfa3c0c5e02e6571baa9aa603542b0534abda
Puneetchauhan9 <pc3622433@gmail.com>
2024-05-26 03:26:49 +0000

    datatypesummmary

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,38 @@
+//Primitive Data type : Are Of 7 Types:
+// Number , bigInt , Null , Undefined , String , Boolean , Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const OutsideTemp = null
+
+let userEmail;
+//console.log(typeof scoreValue);
+
+const  Id = Symbol("235")
+const anotherId = Symbol("235")
+//console.log(Id === anotherId);
+// jo bhi symbol melikhe ge vo kabhi same nhi hoga because symbol ka kaam hi yahi hh
+
+//const bigNumber = 45678654736273n//n is used for to convert number into bigINt
+//console.log(typeof bigNumber);
+
+
+
+
+// Reference or Non Primitive Data type:
+// Array , Function , Object
+
+const Bhagwan = ["shiv" , "bramha" ,"vishnu"];
+
+ let myObj = {
+           Name : "puneet",
+           age: 20
+}
+
+const myFunction = function() {
+
+    console.log("puneet chauhan")
+    
+}
