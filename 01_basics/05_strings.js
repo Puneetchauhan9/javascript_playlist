c2afe4204ea6029620e784cfd80df3dc8b7a0087
Puneetchauhan9 <pc3622433@gmail.com>
2024-05-26 06:02:16 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+let sentence = "my name is puneet singh chauhan"
+
+ //let index = 5;
+
+ //console.log(`the index of ${index} returns the character ${sentence.at(index)}`)
+ //expected output is m.
+
+ let sentence1 ="Man are always brave"
+
+// let index = 1;
+
+ //console.log(`The character is ${sentence1.charAt(index)}`)
+ //expected output is a
+
+
+let index = 4;
+console.log(`Charcter code  ${sentence1.charCodeAt(index)} is equal to ${sentence1.charAt(index)}`)
+//expected outpur:charcter code 97 is equal to a.
+  
+
+
+
