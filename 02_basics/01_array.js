6866728dc962d35ce47897993b22826fff7e36ac
Puneetchauhan9 <pc3622433@gmail.com>
2024-05-30 14:02:22 +0000

    01_array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -18,3 +18,39 @@ console.log(newArray1.pop()) //last array is deleted..
 let anA= newArray1.unshift(7)
  console.log(newArray1.shift());//first ko delete...
  console.log(newArray1);
+
+
+console.log(newArray1.includes(8))  //include is used to check whether the array has 8 or not...
+//return true or false..
+
+console.log(newArray1.indexOf('puneet')); // -1 gives array not found...
+
+console.log(newArray1.indexOf(2))
+//expected output is 1...
+
+// console.log(newArray1.join()); //seprate the array using commas...
+// console.log(newArray1.join(''));//' ' is used to print whole array in one
+// console.log(newArray1.join("-"));//its work is to seprate using commas if not given if given 
+//then uses given seprator...
+
+
+//*******************important methods of array called slice and splice...******************
+//slice  can use to print given(1 ,3) to 2 , 3..
+//last index cannot include
+//or can not change in whole array..array is same....
+
+//whereas splice can be used as given (1 ,3) print 1 , 2, 3......
+//****or in array print whole array except 1 , 2 ,3......
+
+// console.log(newArray1);
+// console.log(newArray1.slice(1 , 3)); //expected output 2 ,3....
+// console.log(newArray1);//array cannot change expected output 1 , 2,3 ,4
+
+let newArray2 = new Array('puneet','himender','chetan','jatin','khushi','yaman','shiv')
+console.log(newArray2);
+console.log(newArray2.splice(1 , 3));//expected output [himender,chetan, jatin]...because himender 1 index
+//chetan is on 2 index and jatin is one 3 index.... 
+//splice include last 
+console.log(newArray2); //expected output all array except [himender,chetan,jatin]...
+
+
