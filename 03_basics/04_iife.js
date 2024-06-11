350b7634b55553c3cfafb1a657a5da5bc5d51847
Puneetchauhan9 <pc3622433@gmail.com>
2024-06-11 05:30:11 +0000

    04_iife function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,19 @@
+//IIFE STANDS FOR IMEDIATE INVOKED FUNCTION EXPRESSION...
+
+// function chai(){
+//     console.log('puneet chauhan');
+// }
+ 
+// chai()  //simple function execution...
+
+//  (function chai(){     
+//     console.log('puneet chauhan');
+// }) ()  // function execution with iife and name function..
+
+//********************** Function call without name with arrow function iife...*********
+
+//  ((name)=> {
+//     console.log(name);
+//  }) ('puneet chuahan')   iife on arrow function....
+
+
