09e4ea0e1d2c25ceac9a457836319790ebc2c04f
Puneetchauhan9 <pc3622433@gmail.com>
2024-06-13 09:38:43 +0000

    truthy_falsy

---
+++ CONTROL_FLOW/truthy_falsy.js
@@ -0,0 +1,18 @@
+const userEmail = []
+// if(userEmail){
+//     console.log("got user email");
+// }
+// else {
+//     console.log("don't get user email");
+// }
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN  all taken as falsy value 
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+ if (userEmail.length == 0) {
+     console.log("Array is empty");
+ }
+
+
