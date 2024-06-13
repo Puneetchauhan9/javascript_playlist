448a160e0c999e222a049d5713386e2f8daef774
Puneetchauhan9 <pc3622433@gmail.com>
2024-06-13 09:21:36 +0000

    switch.js

---
+++ CONTROL_FLOW/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } syntax
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    default:
+        console.log("not a given month");
+        break;
+}
