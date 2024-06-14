c486e2703dfc1a7f685693c233fa3be968911d7c
Puneetchauhan9 <pc3622433@gmail.com>
2024-06-14 15:28:06 +0000

    one.js

--- 05_iterators.js/one.js
+++ 05_iterators.js/one.js
@@ -1,5 +1,50 @@
 // for loop
 
-for(let i = 0; i<=10; i++){
-    console.log(i);
+// for(let i = 0; i<=10; i++){
+//     console.log(i);
+// }
+
+
+for (let i = 1;  i<=10; i++){
+    //console.log(`outer loop is ${i}`);
+   for(let j =1; j<=10; j++){
+    //console.log(`inner loop is ${j} and outer loop is ${i}`);
+    //console.log(i + '*' + j +' = '  +i*j );
+   } 
 }
+
+ const myArray = ['Dhunibaba','Kirmada','Bheem']
+//  let arrayLength =myArray.length
+//  console.log(arrayLength);
+ for(let i = 0; i<myArray.length; i++){
+    // console.log(i);
+    let arrayAccess = myArray[i]
+    //console.log(arrayAccess);
+ }
+
+//********************************** BREAK AND CONTINUE********************************
+
+for (let i = 1; i<=10; i++){
+    if(i ==4)
+        break;
+ //console.log(i);
+}
+
+
+for (let i = 1; i<=10; i++){
+    if(i ==4){
+console.log("detected four");
+        continue;
+    }
+ console.log(`value of i is ${i}`);
+}
+
+
+
+
+
+
+
+
+
+
