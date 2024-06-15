b34a53fb1a663c20d37530ba592785460d7f2c6d
Puneetchauhan9 <pc3622433@gmail.com>
2024-06-15 06:10:03 +0000

    two.js

---
+++ 05_iterators.js/two.js
@@ -0,0 +1,26 @@
+// // while
+// let index =0
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index = index+2
+// }
+
+
+// let myHeroes = ["puneet","jatin","Khushi"]
+
+// let arr = 0
+// while(arr<myHeroes.length){
+//     console.log(`value of myHeroes is ${myHeroes[arr]}`)
+//     arr = arr+1
+// }
+
+let score = 1
+
+do{
+    console.log(`score is : ${score}`)
+    score++
+}while(score<11);
+
+
+
+
